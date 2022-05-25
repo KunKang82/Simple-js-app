8295a43bd1a897c4f24d2e9713ba6c7ab6e26968
KunKang82 <96046913+KunKang82@users.noreply.github.com>
2022-05-25 04:53:04 -0400

    Task 1.2

---
+++ Full-Stack Immersion JavaScript/Achivement1/1.2/js/scripts.js
@@ -0,0 +1,8 @@
+let pokemonList = [
+    {name: 'Bulbasur', height: '0.7', types: ['grass', 'poison']},
+    {name: 'Ivysaur', height: '1.0', types: ['grass', 'poison']},
+    {name: 'Pikachu', height: '0.4', types: ['electric']},
+    {name: 'Raichu', height: '0.8', types: ['electric']},
+    {name: 'Golem', height: '1.4', types: ['rock', 'ground']},
+    {name: 'Rhydon', height: '1.9', types: ['rock', 'ground']}
+];
