69f18013caa0880eb2ef4ee66da97056ce0bc084
KunKang82 <96046913+KunKang82@users.noreply.github.com>
2022-05-28 19:27:00 -0400

    Task 1.3

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,17 @@ let pokemonList = [
     {name: 'Golem', height: 1.4, types: ['rock', 'ground']},
     {name: 'Rhydon', height: 1.9, types: ['rock', 'ground']}
 ];
+
+for (let i=0; i<pokemonList.length; i++){
+    document.write(pokemonList[i].name +" (height: " + pokemonList[i].height +")")    
+    if(pokemonList[i].height>=0.0 && pokemonList[i].height<0.8){
+        document.write(" is a small size.")
+        }
+    else if(pokemonList[i].height>=0.8 && pokemonList[i].height<1.4){
+        document.write(" is a medium size.")    
+    }
+    else{
+        document.write(" is a large size.")
+    }
+}
+
